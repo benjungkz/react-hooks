73c4b78cbbb88ff251462d59bac33495f6d6ef22
Ben Jung <benjungkz@gmail.com>
2020-05-28 19:13:45 -0400

    Hook - useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  // Hook
+  const [ item, setItem ] = useState(1);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const countUpItem = () => setItem( item + 1 );
+  const countDownItem = () => setItem( item - 1 );
+
+  return(
+    <div className="App">
+      <h2>1,2,3 I Like to count, Count with me!!</h2>
+      <h1>{ item }</h1>
+      <button onClick={ countUpItem }>UP</button>
+      <button onClick={ countDownItem }>DOWN</button>
+    </div>
+  );
+}
+
+ReactDOM.render( <App />, document.getElementById('root'));
